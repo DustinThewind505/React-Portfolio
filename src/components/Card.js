b86718f507ae6165b84b92f2bca391caf42a869d
unknown <Dustin.Guillen14@yahoo.com>
2020-04-05 23:22:46 -0600

    fixed bug, using one axios and one fetch. Need to find best practice

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from "axios";
 
 import './Card.css'
@@ -21,37 +21,46 @@ const Card = props => {
 
     useEffect(() => {
 
-        axios.get(`https://api.github.com/users/DustinThewind505/followers`)
-        .then(response => {
-            console.log(response);
-            setRepoData(response)
-        })
+        fetch("https://api.github.com/users/DustinThewind505/followers")
+            .then(res => res.json())
+            .then(response => {
+                console.log(response);
+                setRepoData(response)
+            })
 
 
     }, [])
 
     return (
-        <section key={githubData.id}>
-            <div>
-                <img src="https://dustinthewind505.github.io/images/github.png" alt="Github logo"/>
-            </div>
-            <div>
-                <h3>{githubData.name}</h3>
-                <img src={githubData.avatar_url} alt="github profile"/>
-                <p>Portfolio: <a href={githubData.blog} target="_blank" rel="noopener noreferrer">{githubData.blog}</a></p>
-            </div>
-            <div>
-                <h4>{githubData.login}</h4>
-                <h5>{githubData.bio}</h5>
-                <h6>{githubData.location}</h6>
-                <p>Followers: {githubData.followers}</p>
-                <p>Following: {githubData.following}</p>
-                <p>Repos: {githubData.public_repos}</p>
-                <p>Joined: {githubData.created_at}</p>
-                <p>Last Updated: {githubData.updated_at}</p>
-            </div>
-
-        </section>
+        <Fragment>
+            <section key={githubData.id}>
+                <div>
+                    <img src="https://dustinthewind505.github.io/images/github.png" alt="Github logo" />
+                </div>
+                <div>
+                    <h3>{githubData.name}</h3>
+                    <img src={githubData.avatar_url} alt="github profile" />
+                    <p>Portfolio: <a href={githubData.blog} target="_blank" rel="noopener noreferrer">{githubData.blog}</a></p>
+                </div>
+                <div>
+                    <h4>{githubData.login}</h4>
+                    <h5>{githubData.bio}</h5>
+                    <h6>{githubData.location}</h6>
+                    <p>Followers: {githubData.followers}</p>
+                    <p>Following: {githubData.following}</p>
+                    <p>Repos: {githubData.public_repos}</p>
+                    <p>Joined: {githubData.created_at}</p>
+                    <p>Last Updated: {githubData.updated_at}</p>
+                </div>
+            </section>
+            <section>
+                <div>
+                    {repoData.map(element => (
+                        <p>{element.login}</p>
+                    ))}
+                </div>
+    </section>
+        </Fragment>
     )
 }
 
