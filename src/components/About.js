0551590b968c556a9bddd88e63a0d70bf6d01d11
unknown <Dustin.Guillen14@yahoo.com>
2020-04-10 15:51:30 -0600

    stlying about section

--- src/components/About.js
+++ src/components/About.js
@@ -6,13 +6,19 @@ const About = () => {
 
     return (
         <section className="about-section">
-            <h1>Ello Poppet</h1>
-            <div>
+
+            <div className="about-section-left">
+                <h2>
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user" class="svg-inline--fa fa-user fa-w-14 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z"></path></svg>
+                    About
+                </h2>
                 <p>
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                 <br />
                     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
                 </p>
+            </div>
+            <div className="about-section-right">
                 <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/91212fe7-db10-4f35-8333-ca7c99160630/d2ue8bo-a0b85eee-cd0e-4a4b-9539-d75e2c61cebb.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTpmaWxlLmRvd25sb2FkIl0sIm9iaiI6W1t7InBhdGgiOiIvZi85MTIxMmZlNy1kYjEwLTRmMzUtODMzMy1jYTdjOTkxNjA2MzAvZDJ1ZThiby1hMGI4NWVlZS1jZDBlLTRhNGItOTUzOS1kNzVlMmM2MWNlYmIucG5nIn1dXX0.uYZCHzxX_ziTa26Rh2IqycCyYIIrOLwsLAc0AXo9i-0" className="App-logo" alt="logo" />
             </div>
         </section>
