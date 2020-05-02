33991ea3b808e940802694656cb0df693d5b0fff
unknown <Dustin.Guillen14@yahoo.com>
2020-05-02 16:45:27 -0600

    added spiners

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,14 +9,14 @@ const Footer = props => {
                 <div className="footer-left">
                     <ul>
                         <h5>Social Media:</h5>
-                        <li><img src="http://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c528.png"/><a href="https://www.linkedin.com/in/dustin-guillen-269307183/">LinkedIn</a></li>
-                        <li><a href="https://github.com/DustinThewind505"><img src="https://www.electronjs.org/images/apps/github-desktop-icon.png"/>Github</a></li>
+                        <li><a href="https://www.linkedin.com/in/dustin-guillen-269307183/"><img src="http://icons.iconarchive.com/icons/yootheme/social-bookmark/512/social-linkedin-box-blue-icon.png" className="soap-logo"/>LinkedIn</a></li>
+                        <li><a href="https://github.com/DustinThewind505"><img src="https://www.electronjs.org/images/apps/github-desktop-icon.png" className="soap-logo"/>Github</a></li>
                         <li><a href="https://codesandbox.io/u/DustinThewind505">CodeSandbox</a></li>
                     </ul>
                 </div>
                 <div className="footer-right">
                     <img class="zia-img" src="https://dustinthewind505.github.io/images/zia-symbol-red-on-yellow.png" alt="New Mexico state flag" />
-                    <small>© Copyright 2020 | Dustin Guillen</small>
+                    <small><span className="soap-logo">©</span> Copyright 2020 | Dustin Guillen</small>
                 </div>
             </footer>
         </Fragment>
