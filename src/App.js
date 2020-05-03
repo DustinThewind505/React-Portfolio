0a4eb9803415a59af8a70b627fcde402459720f2
unknown <Dustin.Guillen14@yahoo.com>
2020-05-03 12:58:52 -0600

    styled home.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-
-// import Header from './components/Header';
 import Site from './components/Site.js/SiteContainer';
 import Home from './components/Home/Home';
-// import About from './components/About/AboutComponent';
-// import Projects from './components/Site.js/Projects/ProjectsComponent';
-// import Footer from './components/Footer'
 
 import './css/index.css';
 
@@ -19,7 +14,6 @@ const App = () => {
       <div className="App">
         <Route exact path="/" component={Home} />
         <Route path="/app" component={Site} />
-        {/* <Route path="/projects" component={Projects}/> */}
       </div>
     </Router>
   );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import Technologies from './Technologies';
 
@@ -6,8 +7,13 @@ import Technologies from './Technologies';
 const Home = () => {
 
 
-    return(
-        <div>
+    return (
+        <div className="home">
+            <div className="home-header">
+            <h1><span className="cursive">DustinGuillen</span>.dev<span className="mouseIcon"></span></h1>
+            <p>Software Developer based in New Mexico with a passion for learning,<br/>creativity, and making the world better through technology.</p>
+            <NavLink to="/app/about/">Explore</NavLink>
+            </div>
             <Technologies />
         </div>
     )
