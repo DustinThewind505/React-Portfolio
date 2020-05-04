290b77440021a302e972ad73120339acff49b9a9
unknown <Dustin.Guillen14@yahoo.com>
2020-05-03 22:08:03 -0600

    polished less files readability

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="home">
             <div className="home-header">
             <h1><span className="cursive">DustinGuillen</span>.dev<span className="mouseIcon"></span></h1>
-            <p>Software Developer based in New Mexico with a passion for learning,<br/>creativity, and making the world better through technology.</p>
+            <p>Software Developer based in New Mexico with a passion for learning, creativity, and making the world better through technology.</p>
             <NavLink to="/app/about/">Explore</NavLink>
             </div>
             <Technologies />
