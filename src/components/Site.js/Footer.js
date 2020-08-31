df24a813d0955b032b8436937262af6c86c346f3
unknown <Dustin.Guillen14@yahoo.com>
2020-08-30 22:52:31 -0600

    added sourcerer.io to footer / hid codesanbox

--- src/components/Site.js/Footer.js
+++ src/components/Site.js/Footer.js
@@ -21,8 +21,11 @@ const Footer = props => {
                         <a href="https://github.com/DustinThewind505" target="_blank" rel="noopener noreferrer">
                             <img src="/images/github.png" alt="github logo" />
                         </a>
-                        <a href="https://codesandbox.io/u/DustinThewind505" target="_blank" rel="noopener noreferrer">
+                        <a href="https://codesandbox.io/u/DustinThewind505" className="codesandbox-logo" target="_blank" rel="noopener noreferrer">
                             <img id="codesandbox-logo" src="/images/Codesandbox.png" alt="code sandbox logo" />
+                        </a>
+                        <a href="https://sourcerer.io/dustinthewind505">
+                            <img src="https://sourcerer.io/icons/logo-sharing.svg" alt="Sourcerer" />
                         </a>
                     </nav>
                 </div>
